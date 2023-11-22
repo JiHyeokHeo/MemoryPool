b64b1fe3fb560b6b763ccdef3d730d48f7736586
JiHyeok Heo <vndj1212@gmail.com>
2023-11-22 20:01:42 +0900

    Pointer vs Reference

--- MemoryPool/Pointer/Reference.cpp
+++ MemoryPool/Pointer/Reference.cpp
@@ -39,15 +39,43 @@ void PrintInfoByCopy(StatInfo info)
 	cout << "-----------------------" << endl;
 }
 
-// 2) 주소 전달 방식
-
-void PrintInfoByPtr(StatInfo* info)
+// 특정 조건을 만족하는 몬스터를 찾는 함수
+StatInfo* FindMonster()
 {
+	// TODO : Heap 영역에서 뭔가를 찾아봄
+	// 찾았다!
+	// return monster~;
+
+	return nullptr;
+}
+
+// 2) 주소 전달 방식
+StatInfo globalInfo;
+void PrintInfoByPtr(const StatInfo* const info)
+{
+	if (info == nullptr)
+		return;
+
 	cout << "-----------------------" << endl;
 	cout << "HP: " << info->hp << endl;
 	cout << "ATT: " << info->attack << endl;
 	cout << "DEF: " << info->defence << endl;
 	cout << "-----------------------" << endl;
+
+	// 별 뒤에 붙인다면?
+	// StatInfo* const info
+	// info라는 바구니의 내용물(주소)을 바꿀 수 없음
+	// info는 주소값을 갖는 바구니 -> 이 주소값이 고정이다.
+
+	// 별 이전에 붙인다면?
+	// const StatInfo* info
+	// info가 '가리키고 있는' 바구니의 내용물을 바꿀 수 없음
+	// '원격' 바구니의 내용물을 바꿀 수 없음
+
+	// info[ 주소값 ]   주소값 [ 데이터 ]
+	//info = &globalInfo;
+
+	//info->hp = 10000;
 }
 
 // StatInfo 구조체가 1000바이트짜리 대형 구조체라면?
@@ -60,13 +88,21 @@ void PrintInfoByPtr(StatInfo* info)
 // 주소 전달처럼 주소값을 이용해 진퉁을 건드리는!
 // 일석이조의 방식!
 
-void PrintInfoByRef(StatInfo& info)
+void PrintInfoByRef(StatInfo& info) 
 {
 	cout << "-----------------------" << endl;
 	cout << "HP: " << info.hp << endl;
 	cout << "ATT: " << info.attack << endl;
 	cout << "DEF: " << info.defence << endl;
 	cout << "-----------------------" << endl;
+
+}
+
+#define OUT 
+
+void ChangeInfo(OUT StatInfo& info)
+{
+	info.hp = 1000;
 }
 
 int main()
@@ -108,5 +144,50 @@ int main()
 
 	// 좀더 깊게 들어가면 차이가 있다. // 성능 자체는 포인터를 넘기는거랑 똑같다.
 	PrintInfoByRef(info);
+
+	// 포인터 vs 참조
+	// 성능 : 똑같음
+	// 편의성 : 참조
+	
+	// 1) 편의성 관려
+	// 편의성이 좋다는게 꼭 장점만은 아니다.
+	// 포인터는 주소를 넘기니 확실하게 원본을 넘긴다는 힌트를 줄 수 있다.
+	// 참조는 자연스럽게 모르고 지나칠 수도 있다.
+
+	// 2) 초기화 여부
+	// 참조 타입은 바구니의 2번쨰 이름 (별칭?)
+	// -> 참조하는 대상이 없으면 안됨
+	// 반면 포인터는 그냥 어떤~ 주소라는 의미
+	// -> 대상이 실존하지 않을 수도 있음
+	// 포인터에서 '없다'는 의미로? = nullptr;
+	// 참조 타입은 이런 nullptr의 개념이 없다.
+
+	StatInfo* pointerStat;
+	//pointerStat = &info;
+	//PrintInfoByPtr(pointerStat);
+
+	StatInfo& referenceStat = info;
+	PrintInfoByRef(referenceStat);
+
+
+	// 그래서 결론은?
+	// 사실 Team By Team... 정해진 답은 없다.
+	// ex) 구글에서 만든 오픈소스를 보면 거의 무조건 포인터 사용
+	// ex) 언리얼 엔진에선 reference도 애용
+
+	// 강사 선호 스타일)
+	// - 없는 경우도 고려해야 한다면 pointer (null 체크 필수)
+	// - 바뀌지 않고 읽는 용도(readonly)만 사용하면 const ref& 
+	// -  그 외 일반적으로 ref (명시적으로 호출할 때 OUT을 붙인다)
+	// -- 단, 다른 사람이 pointer를 만들어놓은걸 이어서 만든다면, 계속 pointer (섞어 사용하진 않는다)
+	ChangeInfo(OUT info);
+
+	// Bonus) 포인터로 사용하던걸 참조로 넘겨주려면?
+	// pointer [ 주소(&info) ]  info[ 데이터 ]
+	PrintInfoByRef(*pointerStat);
+
+	// Bonums) 참조로 사용하던걸 포인터로 넘겨주려면?
+	// pointer [ 주소 ]  reference, info[ 데이터 ]
+	PrintInfoByPtr(&referenceStat);
 	return 0;
 }
