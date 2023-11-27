d477092e94d45730833016b19b1d2cd44fd09687
JiHyeok Heo <vndj1212@gmail.com>
2023-11-27 19:13:23 +0900

    문자열 연습2

--- MemoryPool/Pointer/StringPractice_1.cpp
+++ MemoryPool/Pointer/StringPractice_1.cpp
@@ -28,11 +28,59 @@ void StrCpy(char* desti, const char* orig)
 	desti[cnt] = '\0';
 }
 
-char* StrCat(char* dest, char* src)
+char* StrCat(char* desti, const char* orig)
 {
+	int cnt = 0;
+	while (desti[cnt] != '\0')
+		cnt++;
 
+	int origCnt = 0;
+	while (orig[origCnt] != '\0')
+	{
+		desti[cnt] = orig[origCnt];
+		origCnt++;
+		cnt++;
+	}
+
+	desti[cnt] = '\0';
+
+	return desti;
 }
 
+int StrCmp(char* a, char* b)
+{
+	int cnt = 0;
+
+	while (a[cnt] != '\0' || b[cnt] != '\0')
+	{
+		if (a[cnt] > b[cnt])
+			return 1;
+		if(a[cnt] < b[cnt])
+			return -1;
+		cnt++;
+	}
+	
+	return 0;
+}
+
+void ReverseStr(char* str)
+{
+	int cnt = 0;
+
+	int len = StrLen(str);
+	int idx = len - 1;
+	while (str[cnt] != '\0')
+	{
+		if ((idx - cnt) == cnt)
+			break;
+
+		char temp;
+		temp = str[cnt];
+		str[cnt] = str[idx - cnt];
+		str[idx - cnt] = temp;
+		cnt++;
+	}
+}
 #pragma warning(disable: 4996)
 
 int main()
@@ -41,13 +89,18 @@ int main()
 
 	// [H][e][l][l][o]['\0'][][][][][][][][][][][][][][][]
 	char a[BUF_SIZE] = "Hello";
-	char b[BUF_SIZE] = "World";
+	char b[BUF_SIZE] = "aa";
 	
 	//int len = StrLen(a);
 	//cout << len;
 
-	strcat(a, b);
+	//StrCpy(b, a);
+	//StrCat(a, b);
 
-	int c = 0;
+	//StrCmp(a, b);
+	ReverseStr(a);
+
+	cout << a << endl;
+	
 	return 0;
 }
