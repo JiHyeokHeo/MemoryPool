29f0a2e70636aeff20336ca8d69265455396c68e
JiHyeok Heo <vndj1212@gmail.com>
2024-01-17 18:00:31 +0900

    이진탐색트리

---
+++ C_Plusplus_Study/Algorithm/BinarySearchTree.cpp
@@ -0,0 +1,174 @@
+#include "BInarySearchTree.h"
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void SetCursorPosition(int x, int y)
+{
+	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
+	::SetConsoleCursorPosition(output, pos);
+}
+
+void BInarySearchTree::Print(Node* node, int x, int y)
+{
+	if (node == nullptr)
+		return;
+
+	SetCursorPosition(x, y);
+
+	cout << node->key;
+	Print(node->left, x - (5 / (y + 1)), y + 1);
+	Print(node->right, x + (5 / (y + 1)), y + 1);
+}
+
+void BInarySearchTree::Print_Inorder(Node* node)
+{
+	// 전위 순회 (preorder traverse)
+	// 중위 순회 (inorder)
+	// 후위 순회 (postorder)
+
+	if (node == nullptr)
+		return;
+
+	// 전위 : [중]이 앞에 온다
+	// 중위 : [중]이 중간에 온다
+	// 후위 : [중]이 마지막에 온다
+
+	// 전위 예시
+	cout << node->key << endl;
+	Print_Inorder(node->left);
+	Print_Inorder(node->right);
+}
+
+Node* BInarySearchTree::Search(Node* node, int key)
+{
+	if (node == nullptr || key == node->key)
+		return node;
+
+	if (key < node->key)
+		return Search(node->left, key);
+	else
+		return Search(node->right, key);
+
+	return nullptr;
+}
+
+Node* BInarySearchTree::Search2(Node* node, int key)
+{
+	while (node && key != node->key)
+	{
+		if (key < node->key)
+			node = node->left;
+		else
+			node = node->right;
+	}
+
+	return node;
+}
+
+Node* BInarySearchTree::Min(Node* node)
+{
+	while (node->left)
+		node = node->left;
+
+	return node;
+}
+
+Node* BInarySearchTree::Max(Node* node)
+{
+	while (node->right)
+		node = node->right;
+
+	return node;
+}
+
+Node* BInarySearchTree::Next(Node* node)
+{
+	if (node->right)
+		return Min(node->right);
+
+	Node* parent = node->parent;
+
+	while (parent && node == parent->right)
+	{
+		node = parent;
+		parent = parent->parent;
+	}
+
+	return parent;
+}
+
+void BInarySearchTree::Insert(int key)
+{
+	Node* newNode = new Node();
+	newNode->key = key;
+
+	if (_root == nullptr)
+	{
+		_root = newNode;
+		return;
+	}
+
+	Node* node = _root;
+	Node* parent = nullptr;
+
+	while (node)
+	{
+		parent = node;
+		if (key < node->key)
+			node = node->left;
+		else
+			node = node->right;
+	}
+	
+	newNode->parent = parent;
+
+	if (key < parent->key)
+		parent->left = newNode;
+	else
+		parent->right = newNode;
+
+}
+
+void BInarySearchTree::Delete(int key)
+{
+	Node* deleteNode = Search(_root, key);
+	Delete(deleteNode);
+}
+
+void BInarySearchTree::Delete(Node* node)
+{
+	if (node == nullptr)
+		return;
+
+	if (node->left == nullptr)
+		Replace(node, node->right);
+	else if (node->right == nullptr)
+		Replace(node, node->left);
+	else
+	{
+		// 다음 데이터 찾기
+		Node* next = Next(node);
+		node->key = next->key;
+		Delete(next);
+	}
+}
+
+// u 서브트리를 v서브트리로 교체
+// 그리고 delete u
+void BInarySearchTree::Replace(Node* u, Node* v)
+{
+	if (u->parent == nullptr)
+		_root = v;
+	else if (u == u->parent->left)
+		u->parent->left = v;
+	else
+		u->parent->right = v;
+
+	if (v)
+		v->parent = u->parent;
+
+	delete u;
+
+}
---
+++ C_Plusplus_Study/Algorithm/Treemain.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <vector>
+//#include <list>
+//#include <stack>
+//#include <queue>
+//#include "BInarySearchTree.h"
+//using namespace std;
+//
+//// 이진 탐색 트리 면접 단골문제 !
+//
+//int main()
+//{
+//	BInarySearchTree bst;
+//
+//	bst.Insert(20);
+//	bst.Insert(10);
+//	bst.Insert(30);
+//	bst.Insert(25);
+//	bst.Insert(26);
+//	bst.Insert(40);
+//	bst.Insert(50);
+//
+//	bst.Delete(20);
+//	bst.Delete(26);
+//
+//	bst.Print();
+//
+//	return 0;
+//}
