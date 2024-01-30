177910182e230ee33402b679951ca02fcc8544cf
JiHyeok Heo <vndj1212@gmail.com>
2024-01-30 17:34:40 +0900

    순열 재귀함수

--- C_Plusplus_Study/Test/ttt.cpp
+++ C_Plusplus_Study/Test/ttt.cpp
@@ -8,18 +8,36 @@
 #include <basetsd.h>
 using namespace std;
 
-vector<int> arr = { 1,2,3 };
+int arr[3] = { 1,2,3 };
+
+int n = 3, k = 2;
+
+void print(vector<int> b)
+{
+	for (int i : b)
+		cout << i << " ";
+	cout << "\n";
+}
+
+void combi(int start, vector<int> b)
+{
+	if (b.size() == k)
+	{
+		print(b);
+		return;
+	}
+
+	for (int i = start + 1; i < n; i++)
+	{
+		b.push_back(arr[i]);
+		combi(i, b);
+		b.pop_back();
+	}
+}
 
 int main() 
 {
+	vector<int> b;
 
-	do
-	{
-		for (int i : arr)
-			cout << i << " ";
-
-		cout << "\n";
-	} while (next_permutation(arr.begin(), arr.end()));
-
-
+	combi(-1, b);
 }
