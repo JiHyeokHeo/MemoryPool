9d8695fb2aacc2d1919aa26d53a8e01cb98fd32c
JiHyeok Heo <vndj1212@gmail.com>
2024-01-30 17:30:13 +0900

    next_permutation

--- C_Plusplus_Study/Test/ttt.cpp
+++ C_Plusplus_Study/Test/ttt.cpp
@@ -8,33 +8,18 @@
 #include <basetsd.h>
 using namespace std;
 
-int arr[3] = { 1,2,3 };
-int n = 3, r = 3;
-
-void print()
-{
-	for (int i : arr)
-		cout << i << " ";
-	cout << "\n";
-}
-
-void permutation(int n, int r, int depth)
-{
-	if (r == depth)
-	{
-		print();
-		return;
-	}
-
-	for (int i = depth; i < n; i++)
-	{
-		swap(arr[i], arr[depth]);
-		permutation(n, r, depth + 1);
-		swap(arr[i], arr[depth]);
-	}
-}
+vector<int> arr = { 1,2,3 };
 
 int main() 
 {
-	permutation(n, r, 0);
+
+	do
+	{
+		for (int i : arr)
+			cout << i << " ";
+
+		cout << "\n";
+	} while (next_permutation(arr.begin(), arr.end()));
+
+
 }
