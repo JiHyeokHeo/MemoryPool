012a193613b5e17806641109dcc992074bad0fbc
JiHyeok Heo <vndj1212@gmail.com>
2024-01-30 17:07:44 +0900

    Split 구분자

--- C_Plusplus_Study/Test/ttt.cpp
+++ C_Plusplus_Study/Test/ttt.cpp
@@ -8,49 +8,25 @@
 #include <basetsd.h>
 using namespace std;
 
-int n = 5, k = 3, a[5] = { 1,2,3,4,5 };
-
-void print(vector<int> b)
+vector<string> split(string input, string delimiter)
 {
-    for (int i : b)
-    {
-        cout << i << " ";
-    }
-    cout << '\n';
-}
+	vector<string> ret;
+	long long pos = 0;
+	string token = " ";
 
-void combi(int start, vector<int> b)
+	while ((pos = input.find(delimiter)) != string::npos)
+	{
+		token = input.substr(0, pos);
+		ret.push_back(token);
+		input.erase(0, pos + delimiter.length());
+	}
+	ret.push_back(input);
+
+	return ret;
+}
+int main() 
 {
-    if (b.size() == k)
-    {
-        print(b);
-        return;
-    }
-
-    for (int i = start + 1; i < n; i++)
-    {
-        b.push_back(a[i]);
-        combi(i, b);
-        b.pop_back();
-    }
-
-}
-
-int main() {
-
-    vector<int> b;
-    //combi(-1, b);
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            for (int k = 0; k < j; k++)
-            {
-                cout << i << " : " << j << " : " << k << "\n";
-            }
-        }
-    }
-
-    return 0;
+	string s = "abdc", d = "d";
+	vector<string> a = split(s, d);
+	for (string b : a) cout << b << "\n";
 }
